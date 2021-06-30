b01b3433a317a2025bb656c6616f167867e50041
yang yizhou <yangyizhou99@gmail.com>
2021-06-30 10:43:13 +0200

    fix: add function documentation csv_read

--- src/csv_read.py
+++ src/csv_read.py
@@ -2,6 +2,8 @@ import pandas as pd
 import os
 
 def load_housing_data(path):
+    '''
+        this function can load csv file data
+    '''
     csv_path = os.path.join(path, "10.csv")
     return pd.read_csv(csv_path)
-
