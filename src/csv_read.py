bcc8303cba4d1f11effa3b3b4ec972fbfc35a20c
yang yizhou <yangyizhou99@gmail.com>
2021-06-30 12:28:10 +0200

    fix: csv_read test

--- src/csv_read.py
+++ src/csv_read.py
@@ -1,15 +1,16 @@
 '''
    this file contains function read csv
 '''
-import pandas as pd
 import urllib.request
+import pandas as pd
 
 def fetch_data():
     '''
         this function can fetch the dataset file
     '''
-    DOWNLOAD_ROOT = "https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System%20Data/TSP/10.csv"
-    urllib.request.urlretrieve(DOWNLOAD_ROOT, "10.csv")
+    path = "https://raw.githubusercontent.com/EKU-Summer-2021/intelligent_system_data/main/Intelligent%20System" \
+           "%20Data/TSP/10.csv "
+    urllib.request.urlretrieve(path, "10.csv")
 
 def load_data():
     '''
