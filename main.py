fe5fa2ab4d4a89e742fb32ef041eabf1173b45e2
yang yizhou <yangyizhou99@gmail.com>
2021-06-30 10:56:27 +0200

    fix: csv_read automatic format

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import numpy as np
+
 from src import Polynomial
 from src import csv_read
 
 if __name__ == '__main__':
-    coeffs = np.array([1,0,0])
+    coeffs = np.array([1, 0, 0])
     polynom = Polynomial(coeffs)
     dataset = csv_read.load_housing_data("intelligent_system_data-main\Intelligent System Data\TSP")
     print(polynom.evaluate(3))
--- src/csv_read.py
+++ src/csv_read.py
@@ -1,5 +1,7 @@
-import pandas as pd
 import os
+
+import pandas as pd
+
 
 def load_housing_data(path):
     '''
